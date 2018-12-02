d8b22c600a44882c5e942a3f042c68e23a170436
Dmitry Bayov <dmitry.bajov@gmail.com>
2018-12-02 20:19:28 +0300

    fix driver platform

--- src/test/java/testng/TestBase.java
+++ src/test/java/testng/TestBase.java
@@ -1,39 +1,34 @@
 package testng;
 
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 import ru.stqa.selenium.factory.*;
 import util.PropertyLoader;
 
-import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class TestBase {
 
 
     protected WebDriver driver;
-    protected URL gridHubUrl;
     protected String baseUrl;
     @BeforeClass
     public void init() {
         baseUrl = PropertyLoader.loadProperty("site.url");
-        System.setProperty("webdriver.chrome.driver", "chromedriver_nix");
-        DesiredCapabilities capabilities = new DesiredCapabilities();
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
 
-        capabilities.setBrowserName(PropertyLoader.loadProperty("browser.name"));
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("–no-sandbox");
+        options.addArguments("–disable-dev-shm-usage");
+
+//        capabilities.setBrowserName(PropertyLoader.loadProperty("browser.name"));
 //        capabilities.setVersion(PropertyLoader.loadProperty("browser.version"));
-        String platform = PropertyLoader.loadProperty("browser.platform");
-        if (!(null == platform || "".equals(platform))) {
-            capabilities.setPlatform(Platform.valueOf(PropertyLoader.loadProperty("browser.platform")));
-        }
-        if (!(null == gridHubUrl || "".equals(gridHubUrl))) {
-            driver = SingleWebDriverPool.DEFAULT.getDriver(gridHubUrl, capabilities);
-        } else {
-            driver = SingleWebDriverPool.DEFAULT.getDriver(capabilities);
-        }
+//        String platform = PropertyLoader.loadProperty("browser.platform");
+
+        driver = SingleWebDriverPool.DEFAULT.getDriver(options);
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
     @AfterSuite(alwaysRun = true)
